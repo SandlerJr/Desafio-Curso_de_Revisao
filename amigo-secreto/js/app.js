68265727d355c4c4f1779619557a0b7158134094
SandlerJr <sandlerjunin@gmail.com>
2023-11-12 19:47:12 -0300

    desfio 4: Criamos um aplicação que sorteia amigos secreto com funções para adicionar nome dos participantes, sortea-los, embaralha-los, retorna-los e uma função para reiniciar

---
+++ amigo-secreto/js/app.js
@@ -0,0 +1,43 @@
+let listaAmigos = [];
+
+function adicionar() {
+    let amigo = document.getElementById('nome-amigo');
+    let amigos = document.getElementById('lista-amigos');
+
+    listaAmigos.push(amigo.value);
+
+    if (amigos.textContent == '') {
+        amigos.textContent = amigo.value;
+    } else {
+        amigos.textContent += ', ' + amigo.value;
+    }
+
+    amigo.value = '';
+}
+
+function sortear(){
+    embaralhar(listaAmigos);
+
+    let sorteio = document.getElementById('lista-sorteio');
+    for (let i = 0; i < amigos.length; i++){
+        if (i == amigos.length - 1) {
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] +' --> ' +amigos[0] + '<br/>';
+        } else {
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] +' --> ' +amigos[i + 1] + '<br/>';
+        }
+    }
+}
+
+function embaralhar(lista) {
+    for (let indice = lista.length; indice; indice--) {
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+        [lista[indice - 1], lista[indiceAleatorio]] = [lista[indiceAleatorio], lista[indice - 1]];
+    }
+}
+
+function reiniciar() {
+    listaAmigos = [];
+    document.getElementById('lista-amigos').innerHTML = '';
+    document.getElementById('lista-sorteio').innerHTML = '';
+}
+
